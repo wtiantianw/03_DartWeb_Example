01373165a86bb81d3d034d98834f3deab31a60ba
wtiantianw <1538369187@qq.com>
2022-05-05 17:40:03 +0800

    画柱状图

--- web/my-charts.dart
+++ web/my-charts.dart
@@ -1,7 +1,3 @@
-// Dart JS示例
-// AntV可视化组件（https://antv.antfin.com/zh-cn/g2/3.x/demo/pie/color-rose.html#）
-// Dart JS文档（https://api.dart.cn/stable/2.16.2/dart-js/dart-js-library.html）
-
 import 'dart:html';
 import 'dart:js';
 
@@ -10,11 +6,11 @@ void main(List<String> args) {
   dartStatus.text = 'Dart is running!';
 
   var myData = JsObject.jsify([
-    {'year': '2001', 'population': 2},
-    {'year': '2002', 'population': 4},
-    {'year': '2003', 'population': 6},
-    {'year': '2004', 'population': 8},
-    {'year': '2005', 'population': 10},
+    {'year': '2018', 'sales': 77},
+    {'year': '2019', 'sales': 23},
+    {'year': '2020', 'sales': 34},
+    {'year': '2021', 'sales': 45},
+    {'year': '2022', 'sales': 56},
   ]);
 
   var chart = context['chart'];
